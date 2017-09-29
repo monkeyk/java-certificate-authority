c5ff2fb172be07a0e695e8281e2fabaedc8ec28f
monkeyk7 <zhao@idsmanager.com>
2017-09-29 14:44:18 +0800

    Fix cert issue, add password

--- java-ca-lib/src/main/java/io/github/olivierlemasle/ca/CertificateWithPrivateKey.java
+++ java-ca-lib/src/main/java/io/github/olivierlemasle/ca/CertificateWithPrivateKey.java
@@ -6,21 +6,21 @@ import java.security.PrivateKey;
 
 public interface CertificateWithPrivateKey extends Certificate {
 
-  public KeyStore addToKeystore(KeyStore keyStore, String alias);
+    public KeyStore addToKeystore(KeyStore keyStore, char[] keystorePassword, String alias);
 
-  public KeyStore saveInPkcs12Keystore(String alias);
+    public KeyStore saveInPkcs12Keystore(char[] keystorePassword, String alias);
 
-  public void exportPkcs12(final String keystorePath, final char[] keystorePassword,
-      final String alias);
+    public void exportPkcs12(final String keystorePath, final char[] keystorePassword,
+                             final String alias);
 
-  public void exportPkcs12(final File keystoreFile, final char[] keystorePassword,
-      final String alias);
+    public void exportPkcs12(final File keystoreFile, final char[] keystorePassword,
+                             final String alias);
 
-  public PrivateKey getPrivateKey();
+    public PrivateKey getPrivateKey();
 
-  public String printKey();
+    public String printKey();
 
-  public void saveKey(File file);
+    public void saveKey(File file);
 
-  public void saveKey(String fileName);
+    public void saveKey(String fileName);
 }
